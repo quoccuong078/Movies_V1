f0e967bdbedfd332038183cd01855f13a5adbe27
Cuong <cswork07@gmail.com>
2025-04-09 16:30:28 +0700

    [update] tỉ lệ ảnh + buttom

--- Movies_V1/Controllers/HomePageController.cs
+++ Movies_V1/Controllers/HomePageController.cs
@@ -20,9 +20,9 @@ namespace Movies_V1.Controllers
             var episodes = new List<Episode>
     {
         new Episode { Title = "Tập 1", ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep1" },
-        new Episode { Title = "Tập 2", ThumbnailUrl = "https://static2.vieon.vn/vieplay-image/poster_v4/2025/01/25/pq67xnp1_660x946-quynu.png", WatchUrl = "/watch/ep2" },
-        new Episode { Title = "Tập 3", ThumbnailUrl = "/images/Banner1.jpg", WatchUrl = "/watch/ep3" },
-        new Episode { Title = "Tập 4", ThumbnailUrl = "/images/Banner3.jpg", WatchUrl = "/watch/ep4" },
+        new Episode { Title = "Tập 2", ThumbnailUrl = "https://image.plo.vn/w1000/Uploaded/2025/vocgmvpi/2024_01_04/mai2-2285-4683.jpg.webp", WatchUrl = "/watch/ep2" },
+        new Episode { Title = "Tập 3", ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep3" },
+        new Episode { Title = "Tập 4", ThumbnailUrl = "https://www.elle.vn/wp-content/uploads/2023/12/06/560540/poster-Mai-scaled.jpg", WatchUrl = "/watch/ep4" },
     };
             return View(episodes); 
         }
