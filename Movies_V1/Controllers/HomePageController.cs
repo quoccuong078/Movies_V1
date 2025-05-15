d6661f3e71907bddec3afe362f40a38e8c05ca13
Cuong <cswork07@gmail.com>
2025-05-15 23:05:36 +0700

    [add] watch

--- Movies_V1/Controllers/HomePageController.cs
+++ Movies_V1/Controllers/HomePageController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
 
 namespace Movies_V1.Controllers
 {
@@ -26,34 +25,68 @@ namespace Movies_V1.Controllers
         public ActionResult Detail()
         {
             var episodes = new List<Episode>
-    {
-        new Episode { Title = "TẬP 1", WatchUrl = "/watch/episode1" },
-        new Episode { Title = "TẬP 2", WatchUrl = "/watch/episode2" },
-        new Episode { Title = "TẬP 3", WatchUrl = "/watch/episode3" },
-        new Episode { Title = "TẬP 4", WatchUrl = "/watch/episode4" },
-        new Episode { Title = "TẬP 5", WatchUrl = "/watch/episode5" },
-        new Episode { Title = "TẬP 6", WatchUrl = "/watch/episode6" },
-        new Episode { Title = "TẬP 7", WatchUrl = "/watch/episode7" },
-        new Episode { Title = "TẬP 8", WatchUrl = "/watch/episode8" },
-        new Episode { Title = "TẬP 9", WatchUrl = "/watch/episode9" },
-        new Episode { Title = "TẬP 10", WatchUrl = "/watch/episode10" },
-        new Episode { Title = "TẬP 11", WatchUrl = "/watch/episode11" },
-        new Episode { Title = "TẬP 12", WatchUrl = "/watch/episode12" }
-    };
-            var recommendedMovies = new List<Movie>
-    {
-        new Movie { ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep1" },
-        new Movie { ThumbnailUrl = "https://image.plo.vn/w1000/Uploaded/2025/vocgmvpi/2024_01_04/mai2-2285-4683.jpg.webp", WatchUrl = "/watch/ep2" },
-        new Movie { ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep3" },
-        new Movie { ThumbnailUrl = "https://www.elle.vn/wp-content/uploads/2023/12/06/560540/poster-Mai-scaled.jpg", WatchUrl = "/watch/ep4" }
-    };
+            {
+                new Episode { Title = "TẬP 1", WatchUrl = "/watch/episode1" },
+                new Episode { Title = "TẬP 2", WatchUrl = "/watch/episode2" },
+                new Episode { Title = "TẬP 3", WatchUrl = "/watch/episode3" },
+                new Episode { Title = "TẬP 4", WatchUrl = "/watch/episode4" },
+                new Episode { Title = "TẬP 5", WatchUrl = "/watch/episode5" },
+                new Episode { Title = "TẬP 6", WatchUrl = "/watch/episode6" },
+                new Episode { Title = "TẬP 7", WatchUrl = "/watch/episode7" },
+                new Episode { Title = "TẬP 8", WatchUrl = "/watch/episode8" },
+                new Episode { Title = "TẬP 9", WatchUrl = "/watch/episode9" },
+                new Episode { Title = "TẬP 10", WatchUrl = "/watch/episode10" },
+                new Episode { Title = "TẬP 11", WatchUrl = "/watch/episode11" },
+                new Episode { Title = "TẬP 12", WatchUrl = "/watch/episode12" }
+            };
 
+            var recommendedMovies = new List<Movie>
+            {
+                new Movie { Title = "Tập 1", ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep1" },
+                new Movie { Title = "Tập 2", ThumbnailUrl = "https://image.plo.vn/w1000/Uploaded/2025/vocgmvpi/2024_01_04/mai2-2285-4683.jpg.webp", WatchUrl = "/watch/ep2" },
+                new Movie { Title = "Tập 3", ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep3" },
+                new Movie { Title = "Tập 4", ThumbnailUrl = "https://www.elle.vn/wp-content/uploads/2023/12/06/560540/poster-Mai-scaled.jpg", WatchUrl = "/watch/ep4" }
+            };
+
+            ViewBag.VideoUrl = "https://vip.opstream12.com/share/f2fb9d75af8f3f2eb322ff968e62a324";
+            ViewBag.MovieTitle = "Ngôi Trời Xa Sông";
             ViewBag.RecommendedMovies = recommendedMovies;
-            return View(episodes); 
+
+            return View(episodes);  
+        }
+
+        public ActionResult Watch(string videoUrl, string movieTitle)
+        {
+            var episodes = new List<Episode>
+            {
+                new Episode { Title = "TẬP 1", WatchUrl = "/watch/episode1" },
+                new Episode { Title = "TẬP 2", WatchUrl = "/watch/episode2" },
+                new Episode { Title = "TẬP 3", WatchUrl = "/watch/episode3" },
+                new Episode { Title = "TẬP 4", WatchUrl = "/watch/episode4" },
+                new Episode { Title = "TẬP 5", WatchUrl = "/watch/episode5" },
+                new Episode { Title = "TẬP 6", WatchUrl = "/watch/episode6" },
+                new Episode { Title = "TẬP 7", WatchUrl = "/watch/episode7" },
+                new Episode { Title = "TẬP 8", WatchUrl = "/watch/episode8" },
+                new Episode { Title = "TẬP 9", WatchUrl = "/watch/episode9" },
+                new Episode { Title = "TẬP 10", WatchUrl = "/watch/episode10" },
+                new Episode { Title = "TẬP 11", WatchUrl = "/watch/episode11" },
+                new Episode { Title = "TẬP 12", WatchUrl = "/watch/episode12" }
+            };
+
+            var recommendedMovies = new List<Movie>
+            {
+                new Movie { Title = "Tập 1", ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep1" },
+                new Movie { Title = "Tập 2", ThumbnailUrl = "https://image.plo.vn/w1000/Uploaded/2025/vocgmvpi/2024_01_04/mai2-2285-4683.jpg.webp", WatchUrl = "/watch/ep2" },
+                new Movie { Title = "Tập 3", ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep3" },
+                new Movie { Title = "Tập 4", ThumbnailUrl = "https://www.elle.vn/wp-content/uploads/2023/12/06/560540/poster-Mai-scaled.jpg", WatchUrl = "/watch/ep4" }
+            };
+
+            ViewBag.VideoUrl = videoUrl ?? "https://vip.opstream12.com/share/f2fb9d75af8f3f2eb322ff968e62a324";
+            ViewBag.MovieTitle = movieTitle ?? "Ngôi Trời Xa Sông";
+            ViewBag.Episodes = episodes;
+            ViewBag.RecommendedMovies = recommendedMovies;
+
+            return View();
         }
     }
 }
-//new Movie { Title = "Tập 1", ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep1" },
-// new Movie { Title = "Tập 2", ThumbnailUrl = "https://image.plo.vn/w1000/Uploaded/2025/vocgmvpi/2024_01_04/mai2-2285-4683.jpg.webp", WatchUrl = "/watch/ep2" },
-// new Movie { Title = "Tập 3", ThumbnailUrl = "https://images2.thanhnien.vn/528068263637045248/2024/2/20/special-poster-2-mai-17084211313531000860296.jpg", WatchUrl = "/watch/ep3" },
-// new Movie { Title = "Tập 4", ThumbnailUrl = "https://www.elle.vn/wp-content/uploads/2023/12/06/560540/poster-Mai-scaled.jpg", WatchUrl = "/watch/ep4" },
